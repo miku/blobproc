f71d496cccb3590582f4c653a3f34bb0f7baaec3
Martin Czygan <martin.czygan@gmail.com>
2025-11-03 15:52:11 +0100

    wip: warc pdf detection

--- cmd/blobfetch/main.go
+++ cmd/blobfetch/main.go
@@ -4,11 +4,14 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 
 	warc "github.com/internetarchive/gowarc"
 )
@@ -43,7 +46,44 @@ func main() {
 			if len(uri) == 0 {
 				continue
 			}
+			// fmt.Println(uri)
+			warcContentType := record.Header.Get("Content-Type")
+			if warcContentType != "application/http; msgtype=response" {
+				continue
+			}
+
+			// Get the content length from WARC header
+			contentLengthStr := record.Header.Get("Content-Length")
+			contentLength, err := strconv.ParseInt(contentLengthStr, 10, 64)
+			if err != nil {
+				log.Printf("skipping record with invalid content-length: %v", err)
+				continue
+			}
+
+			// Limit the reader to the WARC content length
+			r := io.LimitReader(record.Content, contentLength)
+
+			resp, err := http.ReadResponse(bufio.NewReader(r), nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer resp.Body.Close()
+			contentType := resp.Header.Get("Content-Type")
+			if contentType != "application/pdf" {
+				continue
+			}
 			fmt.Println(uri)
+			fmt.Println(record.Offset, record.Size)
+			f, err := os.CreateTemp("", "blobproc-pdf-*")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer f.Close()
+			_, err = io.Copy(f, resp.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(f.Name())
 		}
 	default:
 		log.Println("blobfetch")
