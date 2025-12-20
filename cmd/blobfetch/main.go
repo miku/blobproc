62325bab56b2b16b86d2d0bd60bff4d5f7fa6aae
Martin Czygan <martin.czygan@gmail.com>
2025-12-20 13:37:34 +0100

    add filter

--- cmd/blobfetch/main.go
+++ cmd/blobfetch/main.go
@@ -89,12 +89,13 @@ func main() {
 		log.Printf("found %d files in %s", len(item.Files), itemID)
 		// Crawl data items are about 50GB in total, with about 1GB per files,
 		// with 100-10000s of PDF.
-		for _, file := range item.Files {
+		for i, file := range item.Files {
 			if !strings.HasSuffix(file.Name, ".warc.gz") {
 				continue
 			}
 			// https://archive.org/download/OPENALEX-CRAWL-2025-09-20251011130616382-07663-07716-wbgrp-crawl047/OPENALEX-CRAWL-2025-09-20251011144946523-07666-2129926~wbgrp-crawl047.us.archive.org~8443.warc.gz
 			fileURL := fmt.Sprintf("https://archive.org/download/%s/%s", itemID, file.Name)
+			log.Printf("processing file %d/%d: %v", i, len(item.Files), fileURL)
 			resp, err := http.Get(fileURL)
 			if err != nil {
 				log.Fatal(err)
@@ -103,12 +104,14 @@ func main() {
 			extractor := &warcutil.Extractor{
 				Filters: []warcutil.ResponseFilter{
 					warcutil.PDFResponseFilter, // TODO: just PDF as default, but that is not fixed
+					warcutil.NonZeroContentLengthFilter,
 				},
 				Processors: []warcutil.Processor{},
 			}
 			switch {
 			case *verbose:
 				extractor.Processors = append(extractor.Processors, debugProcessor)
+				fallthrough
 			case *outputDir != "":
 				processor := &warcutil.DirProcessor{
 					Dir:       *outputDir,
@@ -116,15 +119,18 @@ func main() {
 					Extension: ".pdf",
 				}
 				extractor.Processors = append(extractor.Processors, processor)
+				fallthrough
 			case *postURL != "":
 				var httpPostProcessor = &warcutil.HttpPostProcessor{
 					URL: *postURL,
 				}
 				extractor.Processors = append(extractor.Processors, httpPostProcessor)
 			}
+			log.Printf("extractor has %d processor(s)", len(extractor.Processors))
 			if err := extractor.Extract(resp.Body); err != nil {
 				log.Fatal(err)
 			}
+			_ = resp.Body.Close()
 		}
 	case *fromWarcFile != "":
 		f, err := os.Open(*fromWarcFile)
